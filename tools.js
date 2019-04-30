dc4c7c58ad53701ef51af769f69216323c266688
E.L <17629249784@163.com>
2019-04-30 16:05:06 +0800

    提交文件

---
+++ tools.js
@@ -0,0 +1,26 @@
+function move(obj,attr,target,speed,callback){
+    clearInterval(timer);
+    var current=parseInt(getStyle(obj,attr));
+    if(current > target){
+        speed=-speed;
+    }
+    obj.timer=setInterval(function(){
+        var oldValue = parseInt(getStyle(obj,attr));
+        var newValue = oldValue + speed;
+        if ((speed<0 && newValue<target) || (speed>0 && newValue>target)){
+            newValue=target;
+        }
+        obj.style[attr]=newValue+"px";
+        if(newValue == target){
+            clearInterval(obj.timer);
+            callback && callback();
+        }
+    },30);
+     function getStyle(obj,name){
+        if(window.getComputedStyle){
+        return getComputedStyle(obj,null)[name];
+    }else{
+        return obj.currenStyle[name];
+        }
+    }
+}
